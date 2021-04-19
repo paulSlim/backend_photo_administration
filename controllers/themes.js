ed157e9fa8f7b8b7255816993aaa6c0ba7aa116d
Paweł Ślimko <pawel.slimko@wp.pl>
2021-04-19 21:08:28 +0200

    modified post theme method

--- controllers/themes.js
+++ controllers/themes.js
@@ -1,55 +1,59 @@
-const { v4: uuid } = require('uuid');
-const fs = require('fs');
+const { v4: uuid } = require("uuid");
+const fs = require("fs");
 
-const themesData = ['pierwszy_temat', 'drugi_temat'];
+const themesData = [];
 
 exports.getThemes = (request, response, next) => {
   try {
     response.status(200).json({
-      themes: themesData
+      themes: themesData,
     });
   } catch (error) {
     response.status(500).json({
       error,
-      message: 'Oops! Coś poszło nie tak, przy metodzie GET w endpointcie /themes',
+      message:
+        "Oops! Coś poszło nie tak, przy metodzie GET w endpointcie /themes",
     });
   }
 };
 
 exports.postTheme = (request, response, next) => {
-    try {
-      const { themeName } = request.body;
-      if (!themeName) {
-        response.status(400).json({
-          message: 'Nie podano nazwy tematu',
-        });
-  
-        return;
-      }
-  
-      const isThemeExist = themesData.some(({ themeName: currentName }) => currentName === title);
-      if (isThemeExist) {
-        response.status(409).json({
-          message: `Istnieje już w bazie temat o podanym tytule ${themeName}`,
-        });
-  
-        return;
-      }
-  
-      const newTheme = {
-          themeName,
-          id: uuid(),
-        };
-  
-      photosData.push(newTheme);
-  
-      response.status(201).json({
-        themes: themesData
+  try {
+    const { themeName } = request.body;
+    if (!themeName) {
+      response.status(400).json({
+        message: "Nie podano nazwy tematu",
       });
-    } catch (error) {
-      response.status(500).json({
-        error,
-        message: 'Oops! Coś poszło nie tak, przy metodzie POST w endpointcie /themes'
-      });
+
+      return;
     }
-  };
+
+    const isThemeExist = themesData.some(
+      ({ themeName: currentName }) => currentName === themeName
+    );
+    if (isThemeExist) {
+      response.status(409).json({
+        message: `Istnieje już w bazie temat o podanym tytule ${themeName}`,
+      });
+
+      return;
+    }
+
+    const newTheme = {
+      themeName,
+      id: uuid(),
+    };
+
+    themesData.push(newTheme);
+
+    response.status(201).json({
+      themes: themesData,
+    });
+  } catch (error) {
+    response.status(500).json({
+      error,
+      message:
+        "Oops! Coś poszło nie tak, przy metodzie POST w endpointcie /themes",
+    });
+  }
+};
--- routes/themes.js
+++ routes/themes.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 
-const themesController = require('../controllers/themes');
+const themesController = require("../controllers/themes");
 
 const router = express.Router();
 
-router.get('/', themesController.getThemes);
-router.post('/', themesController.postTheme);
+router.get("/", themesController.getThemes);
+router.post("/", themesController.postTheme);
 // router.put('/', themesController.putTheme);
 // router.delete('/:id', themesController.deleteTheme);
 router.use((request, response) => response.status(404).end());
